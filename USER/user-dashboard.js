5532d1e704391e16628042261b3920e3e8fe0378
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-03-05 01:31:33 -0600

    asd

--- USER/user-dashboard.js
+++ USER/user-dashboard.js
@@ -644,21 +644,6 @@ document.getElementById('commissionLevel').addEventListener('change', () => {
 
 
 
-function openPage(pageName, element) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tab-link");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(pageName).style.display = "block";
-    element.className += " active";
-}
-// Trigger the first tab to open by default
-document.getElementsByClassName("tab-link")[0].click();
 
 
 
