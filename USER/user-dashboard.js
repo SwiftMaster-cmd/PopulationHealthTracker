ca47210537f18578ba3038bf3040d3225c63c05e
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-04-17 19:34:26 -0500

    l

--- USER/user-dashboard.js
+++ USER/user-dashboard.js
@@ -646,6 +646,7 @@ document.getElementById('commissionLevel').addEventListener('change', () => {
 
 
 
+
 // Global variable to store the current sale data, including timestamp
 let currentSaleData;
 
@@ -683,7 +684,6 @@ document.querySelectorAll('.edit-sale-type-btn').forEach(btn => {
 
 // Event listeners for edit ESI consent buttons
 document.querySelectorAll('.edit-esi-consent-btn').forEach(btn => {
-    btn.removeEventListener('click', toggleButtonSelectedState); // Remove existing event listeners to prevent duplicates
     btn.addEventListener('click', function () {
         // First, remove 'selected' class from all ESI consent buttons
         document.querySelectorAll('.edit-esi-consent-btn').forEach(otherBtn => {
