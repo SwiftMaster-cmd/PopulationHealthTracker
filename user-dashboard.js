49f9d9faee055c21d9c0143c6df702e8cc4db24a
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-05-05 21:49:44 -0500

    dsa

--- user-dashboard.js
+++ user-dashboard.js
@@ -690,6 +690,15 @@ function setupSalesProgressListener(userId) {
             console.log('No goals found');
         }
     });
+}
+
+function updateProgressBars(salesData, goals) {
+    const totals = {
+        "Billable HRA": 0,
+        "Flex HRA":1,
+        "Select RX": 2,
+        "Transfer": 3
+    };
 
     // Aggregate sales data
     Object.values(salesData).forEach(sale => {
