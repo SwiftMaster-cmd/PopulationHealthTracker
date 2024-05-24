903475ec8aa7532364a5a732d9d67394f0d01e7c
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-05-24 12:56:08 -0500

    sdf

--- user-dashboard.js
+++ user-dashboard.js
@@ -1,6 +1,7 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+import { getVertexAI, getGenerativeModel } from "firebase/vertexai-preview";
 import { getDatabase, ref, push, set, onValue, remove, get } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
 // Your app's Firebase project configuration
 const firebaseConfig = {
