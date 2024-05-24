11eb365b049cd7bc58194147aa7985d03c9f08db
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-05-24 13:16:26 -0500

    sda

--- user-dashboard.js
+++ user-dashboard.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
-import { getVertexAI, getGenerativeModel } from "firebase/vertexai-preview";
+
 
 // Your app's Firebase project configuration
 const firebaseConfig = {
@@ -20,26 +20,8 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 const database = getDatabase(firebaseApp);
-// Initialize the Vertex AI service
-const vertexAI = getVertexAI(firebaseApp);
-
-const model = getGenerativeModel(vertexAI, { model: "gemini-1.5-flash-preview-0514" });
 
 
-// Wrap in an async function so you can use await
-async function run() {
-    // Provide a prompt that contains text
-    const prompt = "Write a story about a magic backpack."
-  
-    // To generate text output, call generateContent with the text input
-    const result = await model.generateContent(prompt);
-  
-    const response = result.response;
-    const text = response.text();
-    console.log(text);
-  }
-  
-  run();
 
 // Helper functions for UI interactions
 function getSelectedESIContent() {
