5b31687cf9dfb62c90874ce02330a0304bc17dd4
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-05-24 13:38:08 -0500

    rfew

--- user-dashboard.js
+++ user-dashboard.js
@@ -25,6 +25,53 @@ const auth = getAuth();
 const database = getDatabase();
 
 
+
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.AIzaSyDdanD13Lt1SuBgWDNmUgMaiJ9MMqJqky4);
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello, I have 2 dogs in my house." }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  });
+
+  const msg = "How many paws are in my house?";
+
+  const result = await chat.sendMessage(msg);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
+
+
+
+
+
+
+
+
+
+
+
 // Helper functions for UI interactions
 function getSelectedESIContent() {
     const selectedButton = document.querySelector('.esi-btn.selected');
