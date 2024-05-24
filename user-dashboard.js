2d2ccdd3b002eb6ca33dbbfbde86fc716d2ff78c
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-05-24 16:19:32 -0500

    dfsa

--- user-dashboard.js
+++ user-dashboard.js
@@ -3,7 +3,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebas
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 import { getDatabase, ref, push, set, onValue, remove, get } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
 
-import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Your app's Firebase project configuration
 const firebaseConfig = {
@@ -28,24 +27,25 @@ const database = getDatabase();
 
 
 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Access your API key (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(AIzaSyDdanD13Lt1SuBgWDNmUgMaiJ9MMqJqky4);
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.AIzaSyDdanD13Lt1SuBgWDNmUgMaiJ9MMqJqky4);
+
 
 async function run() {
-  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
-
-  const prompt = "Write a story about a magic backpack."
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
-run();
-
+    // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
+  
+    const prompt = "Write a story about a magic backpack."
+  
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+  
+  run();
 
 
 
