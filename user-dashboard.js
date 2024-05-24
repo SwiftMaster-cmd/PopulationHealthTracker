de81d6461e331059c7eaf74a0c4e61c6f0f7fe7c
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-05-24 16:27:02 -0500

    cads

--- user-dashboard.js
+++ user-dashboard.js
@@ -27,28 +27,6 @@ const database = getDatabase();
 
 
 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.AIzaSyDdanD13Lt1SuBgWDNmUgMaiJ9MMqJqky4);
-
-
-async function run() {
-    // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
-  
-    const prompt = "Write a story about a magic backpack."
-  
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-  }
-  
-  run();
-
-
-
 
 
 
