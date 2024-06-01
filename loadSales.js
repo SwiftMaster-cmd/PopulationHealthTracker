b9717a3723010fe588d21ba968fae22a37cb6e23
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-06-01 13:43:04 -0500

    bhj

--- loadSales.js
+++ loadSales.js
@@ -23,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
             day: '2-digit',
             hour: '2-digit',
             minute: '2-digit',
-            second: '2-digit',
             hour12: true
         });
     }
