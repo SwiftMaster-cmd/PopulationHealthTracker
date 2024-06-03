13d90aab4708b662573bd583a635904a70b27fb3
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-06-02 23:06:53 -0500

    efw

---
+++ AutoTracker/firebase-init.js
@@ -0,0 +1,15 @@
+// firebase-init.js
+document.addEventListener('DOMContentLoaded', function() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBhSqBwrg8GYyaqpYHOZS8HtFlcXZ09OJA",
+        authDomain: "track-dac15.firebaseapp.com",
+        databaseURL: "https://track-dac15-default-rtdb.firebaseio.com",
+        projectId: "track-dac15",
+        storageBucket: "track-dac15.appspot.com",
+        messagingSenderId: "495156821305",
+        appId: "1:495156821305:web:7cbb86d257ddf9f0c3bce8",
+        measurementId: "G-RVBYB0RR06"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+});
