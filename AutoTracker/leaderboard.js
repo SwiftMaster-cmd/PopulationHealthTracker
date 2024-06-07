d285e33522a0e9291bd43764176f8455346e22f3
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-06-07 00:04:16 -0500

    asd

--- AutoTracker/leaderboard.js
+++ AutoTracker/leaderboard.js
@@ -1,3 +1,23 @@
+// Ensuring the DOM content is fully loaded before initializing Firebase
+document.addEventListener('DOMContentLoaded', function() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBhSqBwrg8GYyaqpYHOZS8HtFlcXZ09OJA",
+        authDomain: "track-dac15.firebaseapp.com",
+        databaseURL: "https://track-dac15-default-rtdb.firebaseio.com",
+        projectId: "track-dac15",
+        storageBucket: "track-dac15.appspot.com",
+        messagingSenderId: "495156821305",
+        appId: "1:495156821305:web:7cbb86d257ddf9f0c3bce8",
+        measurementId: "G-RVBYB0RR06"
+    };
+
+    // Initialize Firebase only if it hasn't been initialized
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+});
+
+
 // Global variable declaration
 let salesCountsRef;
 
