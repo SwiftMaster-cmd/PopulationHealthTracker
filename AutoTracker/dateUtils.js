42a5b54e9489a0fd9bfc3efae0158fc2bcb364b9
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-06-15 02:37:31 -0500

    dwa

---
+++ AutoTracker/dateUtils.js
@@ -0,0 +1,31 @@
+// Utility functions for date manipulation
+function formatDate(dateTime) {
+    const date = new Date(dateTime);
+    return date.toLocaleDateString();
+}
+
+function formatTime(dateTime) {
+    const date = new Date(dateTime);
+    return date.toLocaleTimeString();
+}
+
+function formatDateTime(dateTime) {
+    const date = new Date(dateTime);
+    return date.toLocaleString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+}
+
+Date.prototype.getWeekNumber = function() {
+    const d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+};
