91e4e88e33bda06a23ae6e922baef78aeca8724c
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-06-20 06:57:35 -0500

    j

--- AutoTracker/leaderboard.js
+++ AutoTracker/leaderboard.js
@@ -46,7 +46,7 @@ function loadLeaderboard(period = 'day', saleType = 'selectRX') {
                     for (const userId in salesData) {
                         const userData = salesData[userId];
                         const count = userData[period] && userData[period][saleType] ? userData[period][saleType] : 0;
-                        const email = usersData[userId] && usersData[userId].email ? usersData[userId].email.split('@')[0] : 'Unknown User';
+                        const email = usersData && usersData[userId] ? usersData[userId].email.split('@')[0] : 'Unknown User';
                         users.push({ email, count });
                     }
 
