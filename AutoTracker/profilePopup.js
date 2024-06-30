f5a0877044ad72b63a0eb55d446b83d999685f05
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-06-30 02:34:28 -0500

    sda

--- AutoTracker/profilePopup.js
+++ AutoTracker/profilePopup.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const profileButton = document.querySelector('.profile-button');
     const colorPickerContainer = document.getElementById('colorPickerContainer');
+    const signOutButton = document.getElementById('signOut');
 
     profileButton.addEventListener('click', () => {
         colorPickerContainer.style.display = colorPickerContainer.style.display === 'none' ? 'block' : 'none';
+    });
+
+    signOutButton.addEventListener('click', () => {
+        firebase.auth().signOut().then(() => {
+            window.location.href = '/Users/micah/Documents/GitHub/PopulationHealthTracker';
+        }).catch((error) => {
+            console.error('Sign out error:', error);
+        });
     });
 });
