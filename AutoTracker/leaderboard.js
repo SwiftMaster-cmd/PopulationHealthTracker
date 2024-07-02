fee4506e14798ab9c59ed58a23fa73e4f47c8519
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-07-02 07:01:53 -0500

    hj

--- AutoTracker/leaderboard.js
+++ AutoTracker/leaderboard.js
@@ -118,8 +118,6 @@ function loadLeaderboard(period = 'day', saleType = 'selectRX') {
 
 
 
-
-
 document.addEventListener('DOMContentLoaded', loadLiveActivities);
 
 async function loadLiveActivities() {
@@ -206,7 +204,11 @@ function renderSales(sales, container, likesRef) {
 
         saleElement.innerHTML = `
             <span class="like-count">0</span>
-            <button class="like-button" data-like-path="${likePath}">❤️</button>
+            <button class="like-button" data-like-path="${likePath}">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+                </svg>
+            </button>
             <strong>${sale.userName}</strong> sold <strong>${sale.saleType}</strong> at ${sale.formattedTime}
         `;
         container.appendChild(saleElement);
