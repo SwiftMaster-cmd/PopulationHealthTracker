d4b2913b3969345d2eb3b02dcd170b254e019174
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-07-17 04:55:57 -0500

    dwa

--- AutoTracker/liveActivities.js
+++ AutoTracker/liveActivities.js
@@ -105,7 +105,7 @@ function renderSales(sales, container, likesRef, usersRef) {
                     <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
                 </svg>
             </button>
-            <strong>${sale.userName}</strong> sold <strong>${sale.saleType}</strong> at ${sale.formattedTime}
+            <strong>${sale.userName}</strong> sold <strong>${sale.saleType}</strong> (<strong>${sale.leadId}</strong>) at ${sale.formattedTime}
             <button class="copy-button">
                 <i class="fas fa-copy"></i>
             </button>
@@ -132,7 +132,7 @@ function renderSales(sales, container, likesRef, usersRef) {
 
 function handleCopyClick(sale) {
     const tempElement = document.createElement('div');
-    tempElement.innerHTML = `<strong>${sale.userName}</strong> sold <strong>${sale.saleType}</strong> at ${sale.formattedTime}`;
+    tempElement.innerHTML = `<strong>${sale.userName}</strong> sold <strong>${sale.saleType}</strong> (<strong>${sale.leadId}</strong>) at ${sale.formattedTime}`;
     document.body.appendChild(tempElement);
 
     const range = document.createRange();
