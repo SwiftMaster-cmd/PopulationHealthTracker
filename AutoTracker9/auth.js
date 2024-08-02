8fd075107294e9ea47222b6c3f69a257b367a8b8
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-08-01 19:08:10 -0500

    faes

--- AutoTracker9/auth.js
+++ AutoTracker9/auth.js
@@ -2,19 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const auth = firebase.auth();
     const googleProvider = new firebase.auth.GoogleAuthProvider();
 
+    function signInWithGoogle() {
+        auth.signInWithPopup(googleProvider).then((result) => {
+            const user = result.user;
+            console.log('User signed in:', user);
+            window.displaySalesOutcomes(user); // Call the global function
+        }).catch((error) => {
+            console.error('Authentication error:', error);
+            showErrorMessage('loginError', 'Failed to sign in with Google. Please try again.');
+        });
+    }
+
     auth.onAuthStateChanged(user => {
         if (user) {
             console.log('User authenticated:', user);
             window.displaySalesOutcomes(user); // Call the global function
         } else {
-            auth.signInWithPopup(googleProvider).then((result) => {
-                const user = result.user;
-                console.log('User signed in:', user);
-                window.displaySalesOutcomes(user); // Call the global function
-            }).catch((error) => {
-                console.error('Authentication error:', error);
-            }); 
+            signInWithGoogle();
         }
     });
-
 });
