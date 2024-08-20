02caef4ddbb9e7a5c6d865c01778fd3b6e9e8544
SwiftMaster-cmd <147448007+SwiftMaster-cmd@users.noreply.github.com>
2024-08-20 11:21:46 -0500

    daw

--- AutoTracker/leaderboard.js
+++ AutoTracker/leaderboard.js
@@ -235,11 +235,6 @@ function renderMoreSales(container, likesRef, usersRef) {
     lastRenderedIndex += salesToRender.length;
 
     salesToRender.forEach((sale) => {
-        // Remove the oldest activity if there are already 10 displayed
-        if (container.childElementCount >= 10) {
-            container.removeChild(container.firstChild);
-        }
-
         const saleDate = new Date(sale.saleTime);
         const today = new Date();
         const isToday = saleDate.getDate() === today.getDate() &&
@@ -284,7 +279,6 @@ function renderMoreSales(container, likesRef, usersRef) {
         renderMoreSales(container, likesRef, usersRef);
     }
 }
-
 
 
 
